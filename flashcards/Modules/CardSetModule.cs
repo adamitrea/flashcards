aee7f1664c602c723675ce774b62198004c71f91
Adam Gligor <gligor.adam@gmail.com>
2016-07-19 16:57:38 +0300

    view cards from all sets

--- flashcards/Modules/CardSetModule.cs
+++ flashcards/Modules/CardSetModule.cs
@@ -1,19 +1,39 @@
-﻿using Nancy;
+﻿using flashcards.domain;
+using Nancy;
+using System.Dynamic;
+using System.Linq;
 
 namespace flashcards.Modules
 {
 	public class CardSetModule : NancyModule
 	{
-		public CardSetModule() : base("/cardset")
+		public dynamic Model = new ExpandoObject();
+
+		public CardSetModule(ICardSetRepository cardSetRepo) : base("/cardset")
 		{
 			Get["/{id}/learn"] = parameters =>
 			{
-				return View["learn"];
+				CardSet cardSet = cardSetRepo.Load(parameters.id);
+				Model.Card = cardSet.Cards.First();
+				Model.Set = cardSet;
+
+				return View["learn", Model];
 			};
 
 			Post["/{id}/learn"] = parameters =>
 			{
-				return View["learn"];
+				int cardId = int.Parse(Request.Form.cardId);
+				bool correctAnswer = Request.Form.answerCorrect == "1";
+				int setId = parameters.id;
+
+				CardSet cardSet = cardSetRepo.Load(parameters.id);
+				int nextCardId = int.Parse(this.Request.Form.cardid) + 1;
+
+				//chose next card
+				Model.Set = cardSet;
+				Model.Card = cardSet.Cards.FirstOrDefault(card => card.Id == nextCardId) ?? cardSet.Cards.First();
+
+				return View["learn", Model];
 			};
 		}
 	}
